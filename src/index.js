c1a12da7a55d258d8fa6ef335f921801434fc6d8
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-06 21:19:45 +0200

    Render feedback only if feedback actually has been given
    This marks my answer for exercise 1.9.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom'
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
+  if (all === 0) {
+    return (
+      <>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+     </>
+    )
+  }
   return (
     <>
      <h1>statistics</h1>
