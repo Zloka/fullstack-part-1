ba1addd60d552fb3bacdac1b46277a2dc87d45dc
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-06 21:51:40 +0200

    Reset app to the "anecdotes" starting point and add button
    This marks my answer for exercise 1.12.

--- src/index.js
+++ src/index.js
@@ -1,64 +1,29 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({ text, onClick }) => {
-  return <button onClick={onClick}>{text}</button>
-};
-
-const Statistic = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  )
-};
-
-const Statistics = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad;
-  if (all === 0) {
-    return (
-      <>
-      <h1>statistics</h1>
-      <p>No feedback given</p>
-     </>
-    )
-  }
-  return (
-    <>
-      <h1>statistics</h1>
-      <table>
-        <tbody>
-          <Statistic text="good" value={good} />
-          <Statistic text="neutral" value={neutral} />
-          <Statistic text="bad" value={bad} />
-          <Statistic text="all" value={all} />
-          <Statistic text="average" value={((good - bad) / all) || 0} />
-          <Statistic text="positive" value={`${good / all * 100 || 0} %`} />
-         </tbody>
-      </table>
-    </>
-  )
-}
-
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
 
   return (
     <div>
-      <h1>give feedback</h1>
+      {props.anecdotes[selected]}
       <div>
-        <Button text="good" onClick={() => setGood(good + 1)} />
-        <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
-        <Button text="bad" onClick={() => setBad(bad + 1)} />
+        <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>next anecdote</button>
       </div>
-      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
 
-ReactDOM.render(<App />, 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
